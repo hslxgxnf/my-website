5b370a3a73ab8170b05bae8fabb2a97f506a7903
Hoseon Lee <hslxgxnf@gmail.com>
2026-03-05 05:19:58 +0900

    feat: add a template for detail pages

---
+++ templates/detail-page.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+
+import styles from "@/styles/detail-page/page.module.css";
+// import data
+// import ReferenceNav from "@/components/detail-page/ReferenceNav";
+// import ReferenceButton from "@/components/detail-page/ReferenceButton";
+// import HeadingLinkId from "@/components/detail-page/HeadingLinkId";
+// import PreCode from "@/components/detail-page/PreCode";
+// import PageNav from "@/components/detail-page/PageNav";
+
+export const metadata: Metadata = {
+  title: "Something",
+};
+
+export default function SomethingPage() {
+  return (
+    <main className={styles.main}>
+      <aside></aside>
+
+      <article></article>
+
+      <aside></aside>
+    </main>
+  );
+}
