9449598e4f0a92d1199e37f23271723571fb8f8a
Hoseon Lee <hslxgxnf@gmail.com>
2026-03-05 05:18:42 +0900

    chore: add the required word

--- app/web/html/naming-rules/page.tsx
+++ app/web/html/naming-rules/page.tsx
@@ -14,7 +14,7 @@ export default function NamingRulesPage() {
       <article>
         <HeadingLinkId headingNumber={1}>Naming Rules</HeadingLinkId>
         <p>
-          A custom name or a custom value of an attribute should{" "}
+          A custom name or a custom value of an attribute should be{" "}
           <code className="emphasis">kebab-case</code>.
           <br />
           <code className="example">data-is-clicked=&quot;true&quot;</code>{" "}
