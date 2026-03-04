19c94b1dc2889f615f896c15e49697ca1f12bd54
Hoseon Lee <hslxgxnf@gmail.com>
2026-03-05 06:32:07 +0900

    feat: add a template for a data.ts file

---
+++ templates/data.ts
@@ -0,0 +1,31 @@
+import type { Reference, Code, Headings } from "@/types/detail-page/interfaces";
+import { favicons } from "@/assets/favicons/favicons";
+
+export const reference: Reference = [
+  [
+    {
+      url: "",
+      image: "",
+      name: "",
+      title: "???",
+    },
+    {
+      url: "?",
+      image: "favicons.??.image",
+      name: "favicons.??.name",
+      title: "???",
+    },
+  ],
+];
+
+export const code: Code = {
+  language: "javascript",
+  content: ``,
+};
+
+export const headings: Headings = [
+  {
+    tag: "h1",
+    content: "???",
+  },
+];
