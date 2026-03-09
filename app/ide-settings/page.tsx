bb06332e616aff68fceb9d6a617689f5defc388e
Hoseon Lee <hslxgxnf@gmail.com>
2026-03-09 21:18:16 +0900

    feat: update the IDE Settings page

--- app/ide-settings/page.tsx
+++ app/ide-settings/page.tsx
@@ -1,17 +1,24 @@
 import type { Metadata } from "next";
 
+import styles from "@/styles/junction-page/page.module.css";
+import HeadingLinkPage from "@/components/junction-page/HeadingLinkPage";
+
 export const metadata: Metadata = {
   title: "IDE Settings",
 };
 
 export default function Page() {
   return (
-    <main>
-      <aside>Aside1</aside>
-      <section>
-        <h1>JetBrains</h1>
-      </section>
-      <aside>Aside2</aside>
+    <main className={styles.main}>
+      <aside></aside>
+
+      <article>
+        <h1>IDE Settings</h1>
+        <hr />
+        <HeadingLinkPage>JetBrains WebStorm</HeadingLinkPage>
+      </article>
+
+      <aside></aside>
     </main>
   );
 }
