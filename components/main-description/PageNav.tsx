da569886fcba16907c5730a10ed8c8131d5bbb02
Hoseon Lee <hslxgxnf@gmail.com>
2026-06-01 20:05:15 +0900

    feat(PageNav): add scroll-to-top and scroll-to-bottom buttons

--- components/main-description/PageNav.tsx
+++ components/main-description/PageNav.tsx
@@ -2,16 +2,30 @@
 
 import { useState, useRef, useEffect } from "react";
 import Link from "next/link";
+import { FaAngleDoubleUp, FaAngleDoubleDown } from "react-icons/fa";
 
 import styles from "@/styles/main-description-&-list/page.module.scss";
 import handleClickFirstLink from "@/functions/main-description/handleClickFirstLink";
 
+// if (!shouldRender) return null;
+// - Nothing
+// useEffect 1
+// - set...
+// useEffect 2
+// - Nothing
+// useState
+// - shouldRender: true
+// if (headings.length === 1) ...
+// - Page Navigation
+// useEffect 2
+// - Intersection Observer
+
 export default function PageNav() {
   const [shouldRender, setShouldRender] = useState(false);
   const [headings, setHeadings] = useState<HTMLHeadingElement[]>([]);
+  const [isScrollable, setIsScrollable] = useState(false);
   const ulRef = useRef<HTMLUListElement>(null);
 
-  // Flow 2
   useEffect(() => {
     const foundHeadings = Array.from(
       document.querySelectorAll<HTMLHeadingElement>(
@@ -19,22 +33,24 @@ export default function PageNav() {
       ),
     );
 
-    // When an article has two or more headings, this component will be displayed.
     if (foundHeadings.length === 0) {
       console.error("No headings");
-      return;
-    } else if (foundHeadings.length >= 2) {
+    } else if (foundHeadings.length >= 1) {
       setShouldRender(true);
       setHeadings(foundHeadings);
+
+      const docHeight = document.documentElement.scrollHeight;
+      const windowHeight = document.documentElement.clientHeight;
+      const isScrollable = docHeight > windowHeight;
+      setIsScrollable(isScrollable);
     }
   }, []);
 
-  // Flow 3
-  // Flow 5 (Run after setShouldRender(true))
   // Intersection Observer
+  // When an article has two or more headings, the Intersection Observer will be used.
+  // The <article> element must be divided into <section> elements.
   useEffect(() => {
-    if (shouldRender && ulRef.current) {
-      // The <article> element must be divided into <section> elements.
+    if (shouldRender && headings.length >= 2 && ulRef.current) {
       const sections = Array.from(
         document.querySelectorAll<HTMLElement>("body > main > article section"),
       );
@@ -79,48 +95,96 @@ export default function PageNav() {
     }
   }, [shouldRender, headings.length]);
 
-  // Flow 1
   if (!shouldRender) return null;
 
-  // Flow 4 (Run after setShouldRender(true))
-  return (
-    <nav>
-      <header>On this page</header>
-      <main>
-        <ul ref={ulRef}>
-          {headings!.map((heading, index) => {
-            const content = heading.firstElementChild!.textContent;
-            const href = `#${content.replaceAll(" ", "-").toLowerCase()}`;
+  if (headings.length === 1) {
+    if (isScrollable) {
+      return (
+        <nav>
+          <ul>
+            <li>
+              <button
+                onClick={() => {
+                  window.scrollTo({
+                    top: 0,
+                  });
+                }}
+              >
+                <FaAngleDoubleUp />
+              </button>
+              <hr />
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  window.scrollTo({
+                    top: document.documentElement.scrollHeight,
+                  });
+                }}
+              >
+                <FaAngleDoubleDown />
+              </button>
+            </li>
+          </ul>
+        </nav>
+      );
+    } else {
+      return null;
+    }
+  } else {
+    return (
+      <nav>
+        <header>On this page</header>
+        <main>
+          <ul ref={ulRef}>
+            {headings!.map((heading, index) => {
+              const content = heading.firstElementChild!.textContent;
+              const href = `#${content.replaceAll(" ", "-").toLowerCase()}`;
 
-            switch (heading.localName) {
-              case "h1":
-                return (
-                  <li key={index}>
-                    <Link
-                      href={href}
-                      onClick={(event) => handleClickFirstLink(event, href)}
-                    >
-                      {content}
-                    </Link>
-                    <hr />
-                  </li>
-                );
-              case "h2":
-                return (
-                  <li key={index}>
-                    <Link href={href}>{content}</Link>
-                  </li>
-                );
-              case "h3":
-                return (
-                  <li key={index} className={styles.indent}>
-                    <Link href={href}>{content}</Link>
-                  </li>
-                );
-            }
-          })}
-        </ul>
-      </main>
-    </nav>
-  );
+              switch (heading.localName) {
+                case "h1":
+                  return (
+                    <li key={index}>
+                      <Link
+                        href={href}
+                        onClick={(event) => handleClickFirstLink(event, href)}
+                      >
+                        {content}
+                      </Link>
+                      <hr />
+                    </li>
+                  );
+                case "h2":
+                  return (
+                    <li key={index}>
+                      <Link href={href}>{content}</Link>
+                    </li>
+                  );
+                case "h3":
+                  return (
+                    <li key={index} className={styles.indent}>
+                      <Link href={href}>{content}</Link>
+                    </li>
+                  );
+              }
+            })}
+            {isScrollable && (
+              <li>
+                <hr />
+                <button
+                  onClick={() => {
+                    window.scrollTo({
+                      top: document.documentElement.scrollHeight,
+                    });
+                  }}
+                >
+                  <FaAngleDoubleDown />
+                </button>
+              </li>
+            )}
+          </ul>
+        </main>
+      </nav>
+    );
+  }
 }
