350c76cfebf5a3df0f8f1383f11078d0748b1545
Hoseon Lee <hslxgxnf@gmail.com>
2026-06-05 21:04:49 +0900

    content(Prettier)

--- app/(07-web)/web/(01-prettier)/prettier/page.tsx
+++ app/(07-web)/web/(01-prettier)/prettier/page.tsx
@@ -36,7 +36,7 @@ export default function Page() {
         </p>
         <p>
           <Highlight type="simple-code" copy={true}>
-            npm install -D prettier
+            npm i -D prettier
           </Highlight>
         </p>
       </article>
