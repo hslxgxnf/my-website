dddbd5c020b1322fffb5bdea295d5c66f21db006
Hoseon Lee <hslxgxnf@gmail.com>
2026-06-13 11:21:57 +0900

    refactor(readFile): verify stylelint config property order
    Check if property orders in `stylelint.config.ts` and `non-custom-properties.txt` match.
    If a mismatch is found, update `stylelint.config.ts` using paste.

--- functions/main-description/readFile.ts
+++ functions/main-description/readFile.ts
@@ -1,6 +1,7 @@
+import stylelintConfig from "@/stylelint.config";
 import { globby } from "globby";
 import { readFile as nodeReadFile } from "node:fs/promises";
-// import clipboard from "clipboardy";
+import clipboard from "clipboardy";
 
 export default async function readFile(
   fileName:
@@ -9,6 +10,40 @@ export default async function readFile(
     | "custom-properties.txt"
     | "non-custom-properties.txt",
 ) {
+  if (fileName === "stylelint.config.ts") {
+    const stylelintProperties = stylelintConfig.rules[
+      "order/properties-order"
+    ][0] as string[];
+
+    const filePaths = await globby(`**/non-custom-properties.txt`, {
+      cwd: "app",
+      absolute: true,
+      gitignore: true,
+    });
+    if (filePaths.length !== 1) {
+      console.error(`filePaths.length: ${filePaths.length} must be 1.`);
+      return;
+    }
+    const pivot = await nodeReadFile(filePaths[0], "utf-8");
+    const pivotProperties = pivot.split("\n").map((line) => line.trim());
+
+    const isEqual =
+      stylelintProperties.length === pivotProperties.length &&
+      stylelintProperties.every(
+        (stylelintProperty, index) =>
+          stylelintProperty === pivotProperties[index],
+      );
+
+    if (!isEqual) {
+      console.error(
+        "stylelintProperties is not up to date. Update it using paste.",
+      );
+
+      const text = JSON.stringify(pivotProperties, null, 2);
+      await clipboard.write(text);
+    }
+  }
+
   const isConfig = fileName.includes(".config.");
 
   const filePaths = await globby(`**/${fileName}`, {
@@ -21,26 +56,6 @@ export default async function readFile(
     console.error(`filePaths.length: ${filePaths.length} must be 1.`);
     return;
   }
-
-  // The following code copies non-custom properties in a desired format used in the stylelint.config.ts file.
-  // if (fileName === "stylelint.config.ts") {
-  //   const filePaths = await globby(`**/non-custom-properties.txt`, {
-  //     cwd: "app",
-  //     absolute: true,
-  //     gitignore: true,
-  //   });
-  //
-  //   if (filePaths.length !== 1) {
-  //     console.error(`filePaths.length: ${filePaths.length} must be 1.`);
-  //     return;
-  //   }
-  //
-  //   const result = await nodeReadFile(filePaths[0], "utf-8");
-  //   const resultArray = result.split("\n").map((line) => line.trim());
-  //
-  //   const textCopy = JSON.stringify(resultArray, null, 2);
-  //   await clipboard.write(textCopy);
-  // }
 
   return await nodeReadFile(filePaths[0], "utf-8");
 }
