25d47e5960270c1f12154b76f3d14fc9d5a00f11
Hoseon Lee <hslxgxnf@gmail.com>
2026-07-04 12:51:37 +0900

    content(Second Largest)

--- app/(06-coding-tests)/coding-tests/second-largest/data.ts
+++ app/(06-coding-tests)/coding-tests/second-largest/data.ts
@@ -15,30 +15,7 @@ export const reference: Reference = [
   },
 ];
 
-export const problemCode: Code = {
-  language: "text",
-  content: `Given an array of positive integers arr[], return the second largest element from the array.
-If the second largest element doesn't exist then return -1.
-
-Note: The second largest element should not be equal to the largest element.
-
-Examples:
-Input: arr[] = [12, 35, 1, 10, 34, 1]
-Output: 34
-Explanation: The largest element of the array is 35 and the second largest element is 34.
-Input: arr[] = [10, 5, 10]
-Output: 5
-Explanation: The largest element of the array is 10 and the second largest element is 5.
-Input: arr[] = [10, 10, 10]
-Output: -1
-Explanation: The largest element of the array is 10 and the second largest element does not exist.
-
-Constraints:
-2 ≤ arr.size() ≤ 10⁵
-1 ≤ arr[i] ≤ 10⁵`,
-};
-
-export const solutionCode: Code = {
+export const code: Code = {
   language: "python",
   content: `def get_second_largest(arr: list[int]) -> int:
     n: int = len(arr)
--- app/(06-coding-tests)/coding-tests/second-largest/page.tsx
+++ app/(06-coding-tests)/coding-tests/second-largest/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 import styles from "@/styles/main-description-&-list/page.module.scss";
-import { reference, problemCode, solutionCode } from "./data";
+import { reference, code } from "./data";
 import ReferenceNav from "@/components/main/ReferenceNav";
 import ReferenceButton from "@/components/main/ReferenceButton";
 import HeadingLinkId from "@/components/main-description/HeadingLinkId/HeadingLinkId";
@@ -22,18 +22,91 @@ export default function Page() {
       <article>
         <section>
           <ReferenceButton>
-            <HeadingLinkId headingNumber={1} lastUpdated="2026-07-03">
+            <HeadingLinkId headingNumber={1} lastUpdated="2026-07-04">
               Second Largest
             </HeadingLinkId>
           </ReferenceButton>
         </section>
         <section>
           <HeadingLinkId headingNumber={2}>Problem</HeadingLinkId>
-          <Highlight type="complex-code">{problemCode}</Highlight>
+          <p>
+            Given an array of positive integers, return the second largest
+            element from the array. If the second largest element doesn't exist
+            then return -1. In other words, the second largest element should
+            not be equal to the largest element.
+          </p>
+          <section>
+            <HeadingLinkId headingNumber={3}>Examples</HeadingLinkId>
+            <ul>
+              <li>
+                <p>
+                  <Highlight type="simple-code" copy={true}>
+                    [12, 35, 1, 10, 34, 1]
+                  </Highlight>{" "}
+                  &#10140; <Highlight type="simple-code">34</Highlight>
+                </p>
+                <ul>
+                  <li>
+                    <p>
+                      The largest element of the array is 35 and the second
+                      largest element is 34.
+                    </p>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <p>
+                  <Highlight type="simple-code" copy={true}>
+                    [10, 5, 10]
+                  </Highlight>{" "}
+                  &#10140; <Highlight type="simple-code">5</Highlight>
+                </p>
+                <ul>
+                  <li>
+                    <p>
+                      The largest element of the array is 10 and the second
+                      largest element is 5.
+                    </p>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <p>
+                  <Highlight type="simple-code" copy={true}>
+                    [10, 10, 10]
+                  </Highlight>{" "}
+                  &#10140; <Highlight type="simple-code">-1</Highlight>
+                </p>
+                <ul>
+                  <li>
+                    <p>
+                      The largest element of the array is 10 and the second
+                      largest element does not exist.
+                    </p>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <HeadingLinkId headingNumber={3}>Constraints</HeadingLinkId>
+            <ul>
+              <li>
+                <p>
+                  <Highlight type="simple-code">2 ≤ arr.size() ≤ 10⁵</Highlight>
+                </p>
+              </li>
+              <li>
+                <p>
+                  <Highlight type="simple-code">1 ≤ arr[i] ≤ 10⁵</Highlight>
+                </p>
+              </li>
+            </ul>
+          </section>
         </section>
         <section>
           <HeadingLinkId headingNumber={2}>Solution</HeadingLinkId>
-          <Highlight type="complex-code">{solutionCode}</Highlight>
+          <Highlight type="complex-code">{code}</Highlight>
         </section>
       </article>
 
