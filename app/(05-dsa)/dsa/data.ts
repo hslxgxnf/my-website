93450d8cb1098d6da6f59fa9e392d93dbb037d1b
Hoseon Lee <hslxgxnf@gmail.com>
2026-08-25 08:23:34 +0900

    content(DSA)

---
+++ app/(05-dsa)/dsa/data.ts
@@ -0,0 +1,44 @@
+import type { Reference } from "@/types/main/interfaces";
+import { favicons } from "@/assets/main-description/favicons/favicons";
+
+export const reference: Reference = [
+  {
+    target: "DSA (Data Structures and Algorithms)",
+    sites: [
+      {
+        url: "https://www.geeksforgeeks.org/dsa/dsa-tutorial-learn-data-structures-and-algorithms/",
+        favicon: favicons.GeeksforGeeks.image,
+        name: favicons.GeeksforGeeks.name,
+        title: "DSA Tutorial",
+      },
+    ],
+  },
+  {
+    target: "Linear Data Structures",
+    sites: [
+      {
+        url: "https://www.geeksforgeeks.org/dsa/difference-between-linear-and-non-linear-data-structures/",
+        favicon: favicons.GeeksforGeeks.image,
+        name: favicons.GeeksforGeeks.name,
+        title: "Difference between Linear and Non-linear Data Structures",
+      },
+      {
+        url: "https://www.geeksforgeeks.org/dsa/introduction-to-linear-data-structures/",
+        favicon: favicons.GeeksforGeeks.image,
+        name: favicons.GeeksforGeeks.name,
+        title: "Introduction to Linear Data Structures",
+      },
+    ],
+  },
+  {
+    target: "Non-Linear Data Structures",
+    sites: [
+      {
+        url: "https://www.geeksforgeeks.org/dsa/introduction-to-hierarchical-data-structure/",
+        favicon: favicons.GeeksforGeeks.image,
+        name: favicons.GeeksforGeeks.name,
+        title: "Introduction to Hierarchical Data Structure",
+      },
+    ],
+  },
+];
--- app/(05-dsa)/dsa/page.tsx
+++ app/(05-dsa)/dsa/page.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 
 import styles from "@/styles/main-description-&-list/page.module.scss";
+import { reference } from "./data";
 import ReferenceNav from "@/components/main/ReferenceNav";
+import ReferenceButton from "@/components/main/ReferenceButton";
 import HeadingLinkPage from "@/components/main-list/HeadingLinkPage";
 
 export const metadata: Metadata = {
@@ -12,17 +14,23 @@ export default function Page() {
   return (
     <main className={styles.main}>
       <aside>
-        <ReferenceNav />
+        <ReferenceNav reference={reference} />
       </aside>
 
       <article>
-        <h1>DSA (Data Structures and Algorithms)</h1>
+        <ReferenceButton>
+          <h1>DSA (Data Structures and Algorithms)</h1>
+        </ReferenceButton>
         <hr />
-        <h2>Linear Data Structures</h2>
+        <ReferenceButton>
+          <h2>Linear Data Structures</h2>
+        </ReferenceButton>
         <HeadingLinkPage>Array</HeadingLinkPage>
         <HeadingLinkPage>List</HeadingLinkPage>
         <hr />
-        <h2>Non-Linear Data Structures</h2>
+        <ReferenceButton>
+          <h2>Non-Linear Data Structures</h2>
+        </ReferenceButton>
         <hr />
         <h2>Algorithms</h2>
         <HeadingLinkPage>Asymptotic Analysis</HeadingLinkPage>
