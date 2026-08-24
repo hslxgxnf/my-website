ff5318a7296b9fb99154a0c6dd460a1051968780
Hoseon Lee <hslxgxnf@gmail.com>
2026-08-25 08:14:12 +0900

    content(Array)

--- app/(05-dsa)/dsa/array/page.tsx
+++ app/(05-dsa)/dsa/array/page.tsx
@@ -21,19 +21,21 @@ export default function Page() {
 
       <article>
         <ReferenceButton>
-          <HeadingLinkId headingNumber={1} lastUpdated="2026-07-23">
+          <HeadingLinkId headingNumber={1} lastUpdated="2026-08-25">
             Array
           </HeadingLinkId>
         </ReferenceButton>
         <p>
-          Index-based access is fast <Highlight type="math">(O(1))</Highlight>.
+          Index-based data access is fast{" "}
+          <Highlight type="math">(O(1))</Highlight>.
         </p>
         <p>
-          Insertion and deletion are slow{" "}
-          <Highlight type="math">(O(n))</Highlight>, except at the end.
+          Data insertion and deletion are slow{" "}
+          <Highlight type="math">(O(n))</Highlight>, except at the end{" "}
+          <Highlight type="math">(O(1))</Highlight>.
         </p>
-        <p>There are fixed-size arrays and dynamic-size arrays.</p>
-        <p>There are one-dimensional arrays and multi-dimensional arrays.</p>
+        <p>There are fixed-size and dynamic-size arrays.</p>
+        <p>There are one-dimensional and multi-dimensional arrays.</p>
       </article>
 
       <aside>
