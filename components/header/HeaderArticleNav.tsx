7661f212f43108cd53545f9b567bd402a5949515
Hoseon Lee <hslxgxnf@gmail.com>
2026-09-01 11:17:23 +0900

    feat(HeaderArticleNav): add responsive design

--- components/header/HeaderArticleNav.tsx
+++ components/header/HeaderArticleNav.tsx
@@ -3,9 +3,10 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { FaCaretRight, FaChevronUp } from "react-icons/fa";
+import { HiMiniArrowTurnDownRight } from "react-icons/hi2";
 
 import toTitleCase from "@/functions/all/toTitleCase";
-import { useEffect, useLayoutEffect, useRef } from "react";
+import { useRef, useLayoutEffect } from "react";
 
 interface Path {
   href: string;
@@ -15,7 +16,7 @@ interface Path {
 export default function HeaderArticleNav() {
   // Button Click Handlers
   const pathHistoryButtonRef = useRef<HTMLButtonElement>(null);
-  const pathHistoryMenuRef = useRef<HTMLDivElement>(null);
+  const pathHistoryMenuRef = useRef<HTMLUListElement>(null);
   function handlePathHistoryButtonClick() {
     const pathHistoryButton = pathHistoryButtonRef.current;
     if (!pathHistoryButton) {
@@ -90,49 +91,15 @@ export default function HeaderArticleNav() {
   }
 
   useLayoutEffect(() => {
-    const pathHistory = pathHistoryRef.current;
-    if (!pathHistory) {
-      console.error("No pathHistory");
-      return;
-    }
-
-    const main = pathHistory.parentElement!;
-    const buttons = pathHistory.nextElementSibling!.nextElementSibling!;
-    const fixedWidth =
-      parseFloat(window.getComputedStyle(main).gap) +
-      buttons.getBoundingClientRect().width;
-    const pathHistoryWidth = pathHistory.scrollWidth;
-
-    const checkOverflow = () => {
-      const mainWidth =
-        main.getBoundingClientRect().width -
-        parseFloat(window.getComputedStyle(main).paddingLeft) -
-        parseFloat(window.getComputedStyle(main).paddingRight);
-
-      const isOverflow = pathHistoryWidth + fixedWidth > mainWidth;
-
-      if (isOverflow) {
-        pathHistory.classList.add("overflow");
-      } else {
-        pathHistory.classList.remove("overflow");
-      }
-    };
-    checkOverflow();
-    const resizeObserver = new ResizeObserver(() => {
-      checkOverflow();
-    });
-    resizeObserver.observe(main);
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [path]);
-
-  useEffect(() => {
-    // Reset the class attribute after page transition.
+    // Reset the settings from the previous page.
     const pathHistoryButton = pathHistoryButtonRef.current;
     if (!pathHistoryButton) {
       console.error("No pathHistoryButton");
+      return;
+    }
+    const pathHistoryMenu = pathHistoryMenuRef.current;
+    if (!pathHistoryMenu) {
+      console.error("No pathHistoryMenu");
       return;
     }
     const referenceNavButton = referenceNavButtonRef.current;
@@ -146,10 +113,11 @@ export default function HeaderArticleNav() {
       return;
     }
     pathHistoryButton.className = "";
+    pathHistoryMenu.className = "";
     referenceNavButton.className = "";
     pageNavButton.className = "";
 
-    // Check if buttons should be active.
+    // Check active.
     const pathHistory = pathHistoryRef.current;
     if (!pathHistory) {
       console.error("No pathHistory");
@@ -203,11 +171,59 @@ export default function HeaderArticleNav() {
     };
   }, [path]);
 
+  useLayoutEffect(() => {
+    // Check overflow.
+    const pathHistory = pathHistoryRef.current;
+    if (!pathHistory) {
+      console.error("No pathHistory");
+      return;
+    }
+    const pathHistoryButton = pathHistoryButtonRef.current;
+    if (!pathHistoryButton) {
+      console.error("No pathHistoryButton");
+      return;
+    }
+
+    const main = pathHistory.parentElement!;
+    const buttons = pathHistory.nextElementSibling!.nextElementSibling!;
+    const fixedWidth =
+      parseFloat(window.getComputedStyle(main).gap) +
+      buttons.getBoundingClientRect().width;
+    const pathHistoryWidth = pathHistory.scrollWidth;
+
+    const checkOverflow = () => {
+      const mainWidth =
+        main.getBoundingClientRect().width -
+        parseFloat(window.getComputedStyle(main).paddingLeft) -
+        parseFloat(window.getComputedStyle(main).paddingRight);
+
+      const isOverflow = pathHistoryWidth + fixedWidth > mainWidth;
+
+      if (isOverflow) {
+        pathHistory.classList.add("overflow");
+        pathHistoryButton.parentElement!.classList.add("overflow");
+      } else {
+        pathHistory.classList.remove("overflow");
+        pathHistoryButton.classList.remove("overflow");
+        pathHistoryButton.parentElement!.classList.remove("overflow");
+      }
+    };
+    checkOverflow();
+    const resizeObserver = new ResizeObserver(() => {
+      checkOverflow();
+    });
+    resizeObserver.observe(main);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [path]);
+
   return (
     <nav>
-      <aside></aside>
+      <div></div>
 
-      <main>
+      <div>
         <ul ref={pathHistoryRef}>
           {processedPath.map((item, index) => {
             if (index !== processedPath.length - 1) {
@@ -220,20 +236,36 @@ export default function HeaderArticleNav() {
               return <li key={index}>{item.name}</li>;
             }
           })}
-          <li>
-            <button
-              ref={pathHistoryButtonRef}
-              onClick={handlePathHistoryButtonClick}
-            >
-              <FaCaretRight />
-            </button>
-          </li>
         </ul>
 
-        {/*pathHistory mobile version*/}
-        <div ref={pathHistoryMenuRef}>
-          <ul>
-            <li>Hello</li>
+        <div>
+          <button
+            ref={pathHistoryButtonRef}
+            onClick={handlePathHistoryButtonClick}
+          >
+            <FaChevronUp />
+          </button>
+
+          <ul ref={pathHistoryMenuRef}>
+            {processedPath.map((item, index) => {
+              if (index !== processedPath.length - 1) {
+                return (
+                  <li key={index}>
+                    <Link href={item.href}>
+                      <HiMiniArrowTurnDownRight />
+                      <span>{item.name}</span>
+                    </Link>
+                  </li>
+                );
+              } else {
+                return (
+                  <li key={index}>
+                    <HiMiniArrowTurnDownRight />
+                    <span>{item.name}</span>
+                  </li>
+                );
+              }
+            })}
           </ul>
         </div>
 
@@ -248,7 +280,7 @@ export default function HeaderArticleNav() {
             <FaChevronUp />
           </button>
         </div>
-      </main>
+      </div>
     </nav>
   );
 }
