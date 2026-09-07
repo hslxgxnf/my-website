dbdf2f6301ee5fb0b9647835638f4477f5506d11
Hoseon Lee <hslxgxnf@gmail.com>
2026-09-08 05:12:38 +0900

    style(HighlightMath)

--- components/main-description/Highlight/HighlightMath.tsx
+++ components/main-description/Highlight/HighlightMath.tsx
@@ -10,5 +10,5 @@ export default function HighlightMath({ children }: HighlightMathProps) {
     displayMode: false,
   });
 
-  return <span dangerouslySetInnerHTML={{ __html: html }} />;
+  return <span className="math" dangerouslySetInnerHTML={{ __html: html }} />;
 }
