17b55bed0ff5ad4be47aca59891172cfc256e5da
Hoseon Lee <hslxgxnf@gmail.com>
2026-09-08 09:01:32 +0900

    feat(HighlightMath): add copy functionality

--- components/main-description/Highlight/HighlightMath.tsx
+++ components/main-description/Highlight/HighlightMath.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import katex from "katex";
 
 interface HighlightMathProps {
@@ -5,6 +8,59 @@ interface HighlightMathProps {
 }
 
 export default function HighlightMath({ children }: HighlightMathProps) {
+  useEffect(() => {
+    function handleCopy(event: ClipboardEvent) {
+      const selection = window.getSelection();
+      if (!selection || selection.rangeCount === 0) {
+        return;
+      }
+
+      const range = selection.getRangeAt(0);
+      const fragment = range.cloneContents();
+      const tempDiv = document.createElement("div");
+      tempDiv.appendChild(fragment);
+
+      const mathElements = tempDiv.querySelectorAll(".math");
+      if (mathElements.length === 0) {
+        return;
+      }
+      for (const mathElement of mathElements) {
+        const annotation = mathElement.querySelector("annotation");
+        if (!annotation) {
+          console.error("No annotation");
+          return;
+        }
+        mathElement.replaceWith(`$${annotation.textContent}$`);
+      }
+
+      const blockElements = tempDiv.querySelectorAll("h1, h2, h3, p, li, br");
+      blockElements.forEach((blockElement) => {
+        blockElement.append("\n");
+      });
+
+      const formattedText = tempDiv.textContent
+        .split("\n")
+        .map((line) => line.trim())
+        .join("\n")
+        .trimEnd();
+
+      if (event.clipboardData) {
+        event.preventDefault();
+        event.clipboardData.setData("text/plain", formattedText);
+      }
+    }
+
+    if (!document.oncopy) {
+      document.oncopy = handleCopy;
+    }
+
+    return () => {
+      if (document.oncopy) {
+        document.oncopy = null;
+      }
+    };
+  }, []);
+
   const html = katex.renderToString(children, {
     throwOnError: false,
     displayMode: false,
