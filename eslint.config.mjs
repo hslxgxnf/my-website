f50ee3c9e869ace917dc24b9ab2526509577f931
hslxgxnf <hslxgxnf@gmail.com>
2026-09-21 22:48:42 +0900

    refactor: use familiar code for gitignore path calculation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-import { fileURLToPath } from "node:url";
+import path from "path";
 import { defineConfig } from "eslint/config";
 import { includeIgnoreFile } from "@eslint/compat";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+const gitignorePath = path.join(process.cwd(), ".gitignore");
 
 const eslintConfig = defineConfig([
   includeIgnoreFile(gitignorePath),
