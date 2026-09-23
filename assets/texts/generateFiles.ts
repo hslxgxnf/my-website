4f40154ba4c05896b247b71cbc2544c50ff4975e
hslxgxnf <hslxgxnf@gmail.com>
2026-09-23 16:36:38 +0900

    refactor(generateFiles.ts): pre-generate sitemap.xml

--- assets/texts/generateFiles.ts
+++ assets/texts/generateFiles.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import fs from "fs";
+import { loadEnvConfig } from "@next/env";
 
 type PageName = string;
 type FileName = string;
@@ -21,4 +22,80 @@ function generateFiles() {
 
     fs.writeFileSync(targetFilePath, content, "utf8");
   }
+}
+
+loadEnvConfig(process.cwd());
+generateSitemap();
+
+function generateSitemap() {
+  const baseDirPath = path.join(process.cwd(), "app");
+  const allRelativePaths = fs.readdirSync(baseDirPath, {
+    recursive: true,
+  }) as string[];
+
+  const matchedRelativePaths = allRelativePaths.filter((relativePath) => {
+    const isPage = path.basename(relativePath) === "page.tsx";
+
+    const normalizedPath = relativePath.replaceAll("\\", "/");
+    const hasPrivateFolder = normalizedPath
+      .split("/")
+      .some((part) => part.startsWith("_"));
+
+    return isPage && !hasPrivateFolder;
+  });
+
+  const sitemapItems = matchedRelativePaths.map((matchedRelativePath) => {
+    const normalizedPath = matchedRelativePath.replaceAll("\\", "/");
+    let cleanUrl = normalizedPath
+      .split("/")
+      .filter((part) => !part.startsWith("(") && part !== "page.tsx")
+      .join("/");
+    cleanUrl = cleanUrl === "" ? "" : `/${cleanUrl}`;
+    const url = escapeXml(`${process.env.NEXT_PUBLIC_SITE_URL}${cleanUrl}`);
+
+    const fullPath = path.join(baseDirPath, matchedRelativePath);
+    const fileContent = fs.readFileSync(fullPath, "utf8");
+    const matchedLastUpdated = fileContent.match(
+      /lastUpdated=["']([^"']+)["']/,
+    );
+    const fileStats = fs.statSync(fullPath);
+    const lastModified =
+      matchedLastUpdated && matchedLastUpdated[1]
+        ? matchedLastUpdated[1]
+        : fileStats.mtime.toISOString().split("T")[0];
+
+    return `  <url>
+    <loc>${url}</loc>
+    <lastmod>${lastModified}</lastmod>
+  </url>`;
+  });
+
+  const sitemapXml = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${sitemapItems.join("\n")}
+</urlset>`;
+
+  fs.writeFileSync(
+    path.join(process.cwd(), "app", "sitemap.xml"),
+    sitemapXml,
+    "utf8",
+  );
+}
+
+function escapeXml(unsafe: string): string {
+  return unsafe.replace(/[<>&'"]/g, (char) => {
+    switch (char) {
+      case "<":
+        return "&lt;";
+      case ">":
+        return "&gt;";
+      case "&":
+        return "&amp;";
+      case "'":
+        return "&apos;";
+      case '"':
+        return "&quot;";
+      default:
+        return char;
+    }
+  });
 }
